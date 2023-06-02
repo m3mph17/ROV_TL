685ceebb4c0034a77a7a23208030537f0b9c89a9
m3mph17 <ez4genius@gmail.com>
2023-06-02 22:17:23 +0300

    intro

---
+++ ROV_TL/Models/Intro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROV_TL.Models
+{
+    public partial class Intro : Form
+    {
+        public Intro()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Opacity += 0.1d;
+            }
+        }
+    }
+}
